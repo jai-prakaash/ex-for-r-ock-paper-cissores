40508360d0996a88d4192576abc63d78c1c7243c
jai-prakaash <jaisjs123@gmail.com>
2025-01-23 15:05:22 +0530

    add rounds tracking to rock paper scissors game

--- rpc.js
+++ rpc.js
@@ -1,8 +1,9 @@
 let result = "";
 let move = "";
-let win = "0";
-let lose = "0";
-let tie = "0";
+let win = 0;
+let lose = 0;
+let tie = 0;
+let rounds = 0;
 
 function shake() {
   const nom3 = Math.random();
@@ -46,39 +47,60 @@ function scissors() {
   console.log(result);
 }
 
-
 function updateResult() {
-    if ((move === "Rock" && result === "Scissors") || 
-    (move === "Paper" && result === "Rock") || 
-    (move === "Scissors" && result === "Paper")) {
+  if (
+    (move === "Rock" && result === "Scissors") ||
+    (move === "Paper" && result === "Rock") ||
+    (move === "Scissors" && result === "Paper")
+  ) {
     win++;
     console.log(`Win= ${win}`);
-    document.querySelector(".showScore").innerHTML = `Win ${win} Tie ${tie} Lose ${lose}`;
-} else if (move === result) {
+
+    rounds = win + lose + tie;
+
+    document.querySelector(
+      ".showScore"
+    ).innerHTML = `Win ${win} Tie ${tie} Lose ${lose}`;
+
+    document.querySelector(".rounds").innerHTML = `Number of rounds ${rounds}`;
+
+  } else if (move === result) {
     tie++;
     console.log(`tie= ${tie}`);
-    document.querySelector(".showScore").innerHTML = `Win ${win} Tie ${tie} Lose ${lose}`;
-} else {
+
+    rounds = win + lose + tie;
+
+    document.querySelector(
+      ".showScore"
+    ).innerHTML = `Win ${win} Tie ${tie} Lose ${lose}`;
+
+    document.querySelector(".rounds").innerHTML = `Number of rounds ${rounds}`;
+  } else {
     lose++;
     console.log(`lose= ${lose}`);
-    document.querySelector(".showScore").innerHTML = `Win ${win} Tie ${tie} Lose ${lose}`;
-}
 
-}
+    rounds = win + lose + tie;
 
+    document.querySelector(
+      ".showScore"
+    ).innerHTML = `Win ${win} Tie ${tie} Lose ${lose}`;
+
+    document.querySelector(".rounds").innerHTML = `Number of rounds ${rounds}`;
+  }
+}
 
 function reset() {
-    win = "0";
- lose = "0";
- tie = "0";
+  win = 0;
+  lose = 0;
+  tie = 0;
+  rounds = 0;
 
- document.querySelector(".move").innerHTML = `Your Move`;
- document.querySelector(".who").innerHTML = `Computer Move`;
+  document.querySelector(".move").innerHTML = `Your Move`;
+  document.querySelector(".who").innerHTML = `Computer Move`;
 
- document.querySelector(".showScore").innerHTML = `Win ${win} Tie ${tie} Lose ${lose}`;
+  document.querySelector(
+    ".showScore"
+  ).innerHTML = `Win ${win} Tie ${tie} Lose ${lose}`;
+
+  document.querySelector(".rounds").innerHTML = `Number of rounds 0`;
 }
-
-
-
-
-
